c4a14474bec0658b40a2441ffe038819a6cdad45
ConnorYasin <connoryasin@gmail.com>
2025-09-16 16:56:29 -0600

    Adding Student info

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,9 @@
+import StudentInfo from '.student-info/StudentInfo';
 export default function Page() {
   return (
     <main>
       <h1>Shopping List</h1>
+        <StudentInfo />
     </main>
   );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from 'next/link';
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Connor Yasinski</h1>
+      <p>
+        <Link href="https://github.com/ConnorYasin/cprg306-assignments">
+            GitHub Repository
+        </Link>
+      </p>
+    </div>
+  );
+};
