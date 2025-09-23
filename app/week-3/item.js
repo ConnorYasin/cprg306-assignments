91752d69883e28eafe4ff8e3a9e2ae5a6f5c464d
ConnorYasin <connoryasin@gmail.com>
2025-09-23 08:26:40 -0600

    Refactor item list and component structure for Week 3

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,12 +1,10 @@
 const Item = ({name, quantity, category}) => {
   return (
-  <li className="item">
     <div>
         <h3>{name}</h3>
         <p>Quantity: {quantity}</p>
         <p>Category: {category}</p>
     </div>
-  </li>
   );
 };
 
---
+++ app/week-3/page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Item from './item';
+import ItemList from './item-list';
+
+const Page = () => {
+    return (
+        <main>
+            <h1>Shopping List</h1>
+            <ItemList />
+        </main>
+    );
+};
+
+export default Page;
