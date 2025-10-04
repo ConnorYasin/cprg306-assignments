804cadb5a93e169acd8e25fdaf6520da34ca356d
ConnorYasin <ConnorYasin@gmail.com>
2025-10-03 18:13:40 -0600

    Enhance New_Item component with increment and decrement functionality; update page title to reflect purpose

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -1,8 +1,9 @@
 import New_Item from "./new-item.js";
+
 const Page = () => {
     return (
         <main>
-            <h1 className='font-bold text-5xl m-5'>New Item</h1>
+            <h1 className='font-bold text-5xl m-5'>New Item Quantity Changer</h1>
             <New_Item />
         </main>
     );
